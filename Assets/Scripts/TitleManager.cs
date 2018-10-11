79864ad0ba5942818c7c68162cbc7801f7f79342
Akira Kosuda <sayuen0@gmail.com>
2018-10-11 21:20:05 +0900

    initial commit, made GameScene and TitleScene

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void PushStartButton (){
+        SceneManager.LoadScene("GameScene");
+    }
+}
